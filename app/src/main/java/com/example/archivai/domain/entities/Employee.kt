863226f2dffa948620cc47bade4270d8e2e217ea
abdelrahman <abdelrahmankarim7@gmail.com>
2025-05-29 14:48:05 +0300

    refactor: Employee data class

--- app/src/main/java/com/example/archivai/domain/entities/Employee.kt
+++ app/src/main/java/com/example/archivai/domain/entities/Employee.kt
@@ -1,14 +1,9 @@
 package com.example.archivai.domain.entities
 
 data class Employee(
-    val id: Int,
-    val name: String,
-    val surname: String,
     val email: String,
-    val phoneNumber: String,
-    val position: String,
-    val sectionId: Int,
-    val sectionName: String,
-    val sectionSize: Long,
-    val sectionLastModified: String
+    val firstName: String,
+    val id: Int,
+    val imageUrl: Any? = null,
+    val lastName: String
 )
